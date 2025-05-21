9bb824b50eb8ec88fa5fc0903598a977a4ba6d23
Bahadır Tarcan <tarcann.bahadir@gmail.com>
2025-05-21 18:30:39 +0300

    MovieDetailView File create.

---
+++ BlindID_Study_Case/Views/MovieDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  MovieDetailView.swift
+//  BlindID_Study_Case
+//
+//  Created by Bahadır Tarcan on 21.05.2025.
+//
+
+import SwiftUI
+
+struct MovieDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieDetailView()
+}
