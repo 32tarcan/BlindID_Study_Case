22796d6b832cc6bdb03f4e96954844f4e176ded9
Bahadır Tarcan <tarcann.bahadir@gmail.com>
2025-05-21 18:41:51 +0300

    AuthViews File created.

---
+++ BlindID_Study_Case/Views/AuthViews/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  BlindID_Study_Case
+//
+//  Created by Bahadır Tarcan on 21.05.2025.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
---
+++ BlindID_Study_Case/Views/AuthViews/RegisterView.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterView.swift
+//  BlindID_Study_Case
+//
+//  Created by Bahadır Tarcan on 21.05.2025.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegisterView()
+}
