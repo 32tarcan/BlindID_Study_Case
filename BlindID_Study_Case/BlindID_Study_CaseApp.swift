e0313a554dd3b3e7fd18ea82b8d468148f5c78db
Bahadır Tarcan <tarcann.bahadir@gmail.com>
2025-05-21 17:14:08 +0300

    Initial Commit

---
+++ BlindID_Study_Case/BlindID_Study_CaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlindID_Study_CaseApp.swift
+//  BlindID_Study_Case
+//
+//  Created by Bahadır Tarcan on 21.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct BlindID_Study_CaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlindID_Study_Case/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BlindID_Study_Case
+//
+//  Created by Bahadır Tarcan on 21.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
