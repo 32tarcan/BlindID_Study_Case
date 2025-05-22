d0e1c6da3069098975030183df9d54a18219491e
Bahadır Tarcan <tarcann.bahadir@gmail.com>
2025-05-22 03:14:54 +0300

    Add IQKeyboardManager configuration for keyboard handling

--- BlindID_Study_Case/BlindID_Study_CaseApp.swift
+++ BlindID_Study_Case/BlindID_Study_CaseApp.swift
@@ -6,14 +6,25 @@
 //
 
 import SwiftUI
+import IQKeyboardManagerSwift
 
 @main
 struct BlindID_Study_CaseApp: App {
+    init() {
+        setupKeyboardManager()
+    }
+    
     var body: some Scene {
         WindowGroup {
             LoginView()
                 .preferredColorScheme(.dark)
                 .background(Color.black)
         }
+    }
+    
+    private func setupKeyboardManager() {
+        let keyboardManager = IQKeyboardManager.shared
+        keyboardManager.isEnabled = true
+        keyboardManager.resignOnTouchOutside = true
     }
 }
